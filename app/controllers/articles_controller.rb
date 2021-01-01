2d2402f87f94204f327c929320f11be1fd2df473
Y <>
2021-01-01 16:17:13 +0900

    formを表示

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,17 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
