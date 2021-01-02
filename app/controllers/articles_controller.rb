77e361cd383f56e2788943ac265524b5ddbbfc1a
Y <>
2021-01-02 11:43:13 +0900

    add flash message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,12 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article), notice: '保存できました'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new
+    end
   end
 
   private
