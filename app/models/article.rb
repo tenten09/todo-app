644e97346d102b106b01b9e5d68b7ac1bce228a6
Y <>
2021-01-04 15:05:04 +0900

    validation length

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,7 +10,10 @@
 #
 class Article < ApplicationRecord
   validates :title, presence: true
+  validates :title, length: { minimum: 3, maximum: 30 }
+
   validates :content, presence: true
+  validates :content, length: { minimum: 10 }
   # validates :deadeline, presence: true
 
   def display_created_at
